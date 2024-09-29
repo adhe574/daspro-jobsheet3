c9e5665d5bf8c87abede9c5f38dae792d65369ab
adhe574 <adhewidya378@gmail.com>
2024-09-29 20:27:04 +0700

    simpan nilai

--- Siakad01.java
+++ Siakad01.java
@@ -4,32 +4,35 @@ public class Siakad01 {
 
     Scanner sc = new Scanner (System.in);
 
-    String nama, nim;
+    String nama, nim, prodi;
     char kelas;
     byte absen;
-    double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+    double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
     
-    System.out.print("Masukkan nama: ");
+    System.out.println("Masukkan nama: ");
     nama = sc.nextLine();
-    System.out.print("Masukkan NIM: ");
+    System.out.println("Masukkan nim: ");
     nim = sc.nextLine();
-    System.out.print("Masukkan kelas: ");
+    System.out.println("Masukkan prodi: ");
+    prodi = sc.nextLine();
+    System.out.println("Masukkan kelas: ");
     kelas = sc.nextLine(). charAt(0);
-    System.out.print("Masukkan nomor absen: ");
+    System.out.println("Masukkan nomor absen: ");
     absen = sc.nextByte();
-
-    System.out.print("Masukkan nilai kuis: ");
+    System.out.println("Masukkan nilai kuis: ");
     nilaiKuis = sc.nextDouble();
-    System.out.print("Masukkan nilai tugas: ");
+    System.out.println("Masukkan nilai tugas: ");
     nilaiTugas = sc.nextDouble();
-    System.out.print("Masukkan nilai ujian: ");
-    nilaiUjian = sc.nextDouble();
+    System.out.println("Masukkan nilai UTS: ");
+    nilaiUTS = sc.nextDouble();
+    System.out.println("Masukkan nilai UAS: ");
+    nilaiUAS = sc.nextDouble();
 
-    nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+    nilaiAkhir = (nilaiKuis*0.20 + nilaiTugas*0.30 + nilaiUAS*0.35) / 3;
 
-   System.out.print("Mahasiswa dengan nama " + nama + " (NIM " + nim + ")");
-   System.out.print(" Kelas " + kelas + " Absen " + absen);
-   System.out.print(" Nilai Akhir " + nilaiAkhir);
+   System.out.println("Mahasiswa dengan nama: " + nama + " (NIM: " + nim + ")");
+   System.out.println("Kelas: " + kelas + "Absen: " + absen);
+   System.out.println("Nilai Akhir: " + nilaiAkhir);
 
     }
 }
